6deb60e53a05dc8bf5f7befa583cdd818d8689cc
KarimElshaweish <KarimAshrafElshaweish@outlook.com>
2017-10-21 18:20:51 +0200

    2 pages design karim

---
+++ app/src/main/java/com/example/karim/prodigyv1/chose_charchter_layout.java
@@ -0,0 +1,13 @@
+package com.example.karim.prodigyv1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class chose_charchter_layout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chose_charchter_layout);
+    }
+}
