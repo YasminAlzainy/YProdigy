d68c45cb0e09ba40d70cc31c4a41106cd7d5b1ea
KarimElshaweish <KarimAshrafElshaweish@outlook.com>
2017-10-27 14:08:42 +0200

    Yasmin Login using FB

--- app/src/main/java/com/example/karim/prodigyv1/MainActivity.java
+++ app/src/main/java/com/example/karim/prodigyv1/MainActivity.java
@@ -10,18 +10,28 @@ public class MainActivity extends AppCompatActivity {
 
     Button btn_log_in;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle  savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn_log_in=(Button)findViewById(R.id.btn_login);
+        btn_log_in = (Button) findViewById(R.id.btn_login);
         btn_log_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               // Intent intent=new Intent(MainActivity.this,chose_charchter_layout.class);
-                Intent intent1=new Intent(MainActivity.this,map_home_layout.class);
-              // Intent intent2=new Intent(MainActivity.this,.class);
+                // Intent intent=new Intent(MainActivity.this,chose_charchter_layout.class);
+                Intent intent1 = new Intent(MainActivity.this, map_home_layout.class);
+                // Intent intent2=new Intent(MainActivity.this,.class);
                 MainActivity.this.startActivity(intent1);
             }
         });
     }
-}
+
+
+
+    public void SignUpClick(View v)
+    {
+        Intent i = new Intent(MainActivity.this,Sign_up.class);
+        startActivity(i);
+    }
+
+        }
+
--- app/src/main/java/com/example/karim/prodigyv1/Sign_up.java
+++ app/src/main/java/com/example/karim/prodigyv1/Sign_up.java
@@ -1,13 +1,153 @@
 package com.example.karim.prodigyv1;
 
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 
 public class Sign_up extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final String TAG = "EmailPassword";
+    CallbackManager callbackManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
+
+        //FaceBook
+
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
     }
+
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+      mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    public void createAccount(String email , String password)
+    {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(Sign_up.this, "Creating acount faild",
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    public void signIn (String email , String password)
+    {
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(Sign_up.this, "Authantication faild",
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    public void getCurrentUser ()
+    {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            // Name, email address, and profile photo Url
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            Uri photoUrl = user.getPhotoUrl();
+
+            // The user's ID, unique to the Firebase project. Do NOT use this value to
+            // authenticate with your backend server, if you have one. Use
+            // FirebaseUser.getToken() instead.
+            String uid = user.getUid();
+        }
+    }
+
+
+    public void FbClick (View v)
+    {
+        Toast.makeText(Sign_up.this, "FaceBook Sign Up done", Toast.LENGTH_LONG).show();
+
+        //Initial Facebook
+
+
+    }
+
+
+
 }
